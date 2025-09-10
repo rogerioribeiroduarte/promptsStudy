2a2d58d1226a6e26042915da561a35549b9e1196
Rogerio Ribeiro Duarte <rogeriorduarte@rduarte.eti.br>
2025-09-10 21:44:37 +0000

    v0.6

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from langchain.chat_models import init_chat_model
 
 app = Flask(__name__)
-CORS(app)  # habilita CORS em todas as rotas
+CORS(app, resources={r"/*": {"origins": "*"}})  # habilita CORS para todas as rotas
 
 # Rota POST de exemplo
 @app.route("/", methods=["POST"])
